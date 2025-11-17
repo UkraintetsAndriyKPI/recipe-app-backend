279d32a0a5c463c6ee90d9d23880f5ac0d36c74a
AndriyUkrainets <andriy.ukrainetc.school@gmail.com>
2025-11-17 23:01:54 +0200

    feature: recipes new app

--- recipe_app/recipe_app/settings.py
+++ recipe_app/recipe_app/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # REST framework
+    'rest_framework',
+
+    # Django apps
+    'recipes',
 ]
 
 MIDDLEWARE = [
