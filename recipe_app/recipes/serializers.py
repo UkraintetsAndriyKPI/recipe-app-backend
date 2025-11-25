c1c0091e7ebd31db012853e94414030e90c25b16
AndriyUkrainets <andriy.ukrainetc.school@gmail.com>
2025-11-26 00:54:56 +0200

    feature: viewset for tags

---
+++ recipe_app/recipes/serializers.py
@@ -0,0 +1,9 @@
+# serializers.py
+from rest_framework import serializers
+
+from .models import Tag
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
